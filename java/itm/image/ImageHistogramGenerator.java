9a9025404f12882db7afc335383c55d6cb5a562f
Pascal Attwenger <pascal.attwenger@gmail.com>
2014-03-29 20:31:23 +0100

    Finished ImageHistogramGenerator

--- java/itm/image/ImageHistogramGenerator.java
+++ java/itm/image/ImageHistogramGenerator.java
@@ -6,6 +6,11 @@ package itm.image;
  *******************************************************************************/
 
 
+import itm.util.Histogram;
+import itm.util.ImageUtil;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -83,26 +88,27 @@ public class ImageHistogramGenerator {
 		// compose the output file name from the absolute path, a path separator and the original filename
 		File outputFile = new File(output, input.getName() + ".hist.png");
 
-
-		// ***************************************************************
-		//  Fill in your code here!
-		// ***************************************************************
-
 		// load the input image
+		BufferedImage image = ImageUtil.load(input);
 
 		// get the color model of the image and the amount of color components
+		int numColorComponents = image.getColorModel().getNumColorComponents();
 
 		// initiate a Histogram[color components] [bins]
-
-		// create a histogram array histArray[color components][bins]
+		Histogram hist = new Histogram(numColorComponents, bins);
 
 		// read the pixel values and extract the color information
+		int[][] histArray = calculateHistArray(image, bins);
 
 		// fill the array setHistogram(histArray)
+		hist.setHistogram(histArray);
 
 		// plot the histogram, try different dimensions for better visualization
+		BufferedImage histImage = hist.plotHistogram(512, 256);
 
 		// encode and save the image as png 
+		ImageUtil.savePNG(histImage, outputFile);
+		
 		return outputFile;
 	}
 
@@ -125,5 +131,42 @@ public class ImageHistogramGenerator {
 		int bins = Integer.parseInt(args[2]);
 		ImageHistogramGenerator histogramGenerator = new ImageHistogramGenerator();
 		histogramGenerator.batchProcessImages(fi, fo, bins);        
-	}    
+	}
+	
+	private int[][] calculateHistArray(BufferedImage image, int bins) {
+		int numColorComponents = image.getColorModel().getNumColorComponents();
+		
+		// create a histogram array histArray[color components][bins]
+		int[][] histArray = new int[numColorComponents][bins];
+		
+		int width = image.getWidth();
+		int height = image.getHeight();
+		
+		for (int i=0; i<height; i++) {
+			for (int j=0; j<width; j++) {
+				if (numColorComponents == 1) {
+					//treat as greyscale
+					int grey = image.getRGB(j, i) & 0xFF;
+					histArray[0][grey]++;
+					
+				} else if (numColorComponents >= 3) {
+					//treat as RGB
+					Color pixel = new Color(image.getRGB(j, i));
+					
+					int red = pixel.getRed();
+					int green = pixel.getGreen();
+					int blue = pixel.getBlue();
+					
+					histArray[0][red]++;
+					histArray[1][green]++;
+					histArray[2][blue]++;
+					
+				} else {
+					throw new IllegalArgumentException("Invalid number of color components");
+				}
+			}
+		}
+		
+		return histArray;
+	}
 }
