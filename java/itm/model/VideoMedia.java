d7dfca9b0789611509967d29ec3278c9730c41ef
philipp <hiermannp@gmail.com>
2014-05-06 16:59:40 +0200

    Task 2.4 95% coded

--- java/itm/model/VideoMedia.java
+++ java/itm/model/VideoMedia.java
@@ -19,9 +19,20 @@ public class VideoMedia extends AbstractMedia {
 	// ***************************************************************
 
 	/* video format metadata */
+	protected String videoCodec;
+	protected String videoCodecID;
+	protected String videoFrameRate;
+	protected long videoLength;
+	protected int videoHeight;
+	protected int videoWidth;
 
 	/* audio format metadata */
-
+	protected String audioCodec;
+	protected String audioCodecID;
+	protected int audioChannels;
+	protected int audioSampleRate;
+	protected int audioBitRate;
+	
 	/**
 	 * Constructor.
 	 */
@@ -42,6 +53,94 @@ public class VideoMedia extends AbstractMedia {
 	// Fill in your code here!
 	// ***************************************************************
 
+	public String getVideoCodec() {
+		return videoCodec;
+	}
+
+	public void setVideoCodec(String videoCodec) {
+		this.videoCodec = videoCodec;
+	}
+
+	public String getVideoCodecID() {
+		return videoCodecID;
+	}
+
+	public void setVideoCodecID(String videoCodecID) {
+		this.videoCodecID = videoCodecID;
+	}
+
+	public String getVideoFrameRate() {
+		return videoFrameRate;
+	}
+
+	public void setVideoFrameRate(String videoFrameRate) {
+		this.videoFrameRate = videoFrameRate;
+	}
+
+	public long getVideoLength() {
+		return videoLength;
+	}
+
+	public void setVideoLength(long videoLength) {
+		this.videoLength = videoLength / 1000000;
+	}
+
+	public int getVideoHeight() {
+		return videoHeight;
+	}
+
+	public void setVideoHeight(int videoHeight) {
+		this.videoHeight = videoHeight;
+	}
+
+	public int getVideoWidth() {
+		return videoWidth;
+	}
+
+	public void setVideoWidth(int videoWidth) {
+		this.videoWidth = videoWidth;
+	}
+
+	public String getAudioCodec() {
+		return audioCodec;
+	}
+
+	public void setAudioCodec(String audioCodec) {
+		this.audioCodec = audioCodec;
+	}
+
+	public String getAudioCodecID() {
+		return audioCodecID;
+	}
+
+	public void setAudioCodecID(String audioCodecID) {
+		this.audioCodecID = audioCodecID;
+	}
+
+	public int getAudioChannels() {
+		return audioChannels;
+	}
+
+	public void setAudioChannels(int audioChannels) {
+		this.audioChannels = audioChannels;
+	}
+
+	public int getAudioSampleRate() {
+		return audioSampleRate;
+	}
+
+	public void setAudioSampleRate(int audioSampleRate) {
+		this.audioSampleRate = audioSampleRate * 1000;
+	}
+
+	public int getAudioBitRate() {
+		return audioBitRate;
+	}
+
+	public void setAudioBitRate(int audioBitRate) {
+		this.audioBitRate = audioBitRate / 1000;
+	}
+	
 	/* (de-)serialization */
 
 	/**
@@ -61,6 +160,28 @@ public class VideoMedia extends AbstractMedia {
 		// ***************************************************************
 		// Fill in your code here!
 		// ***************************************************************
+		
+		out.println();
+		out.println("Video-Metadata: ");
+		out.println();
+		
+		out.println("videoCodec: " + this.videoCodec);
+		out.println("videoCodecID: " + this.videoCodecID);
+		out.println("videoFrameRate: " + this.videoFrameRate);
+		out.println("videoLength [sec]: " + this.videoLength);
+		out.println("videoHeight [pixel]: " + this.videoHeight);
+		out.println("videoWidth: [pixel]" + this.videoWidth);
+		
+		out.println();
+		out.println("Audio-Metadata");
+		out.println();
+		
+		out.println("audioCodec: " + this.audioCodec);
+		out.println("audioCodecID: " + this.audioCodecID);
+		out.println("audioChannels: " + this.audioChannels);
+		out.println("audioSampleRate [Hz]: " + this.audioSampleRate);
+		out.println("audioBitRate [kb/s]: " + this.audioBitRate);
+		
 		return data.getBuffer();
 	}
 
@@ -77,6 +198,7 @@ public class VideoMedia extends AbstractMedia {
 		while ((line = br.readLine()) != null) {
 
 			/* video fields */
+			
 			// ***************************************************************
 			// Fill in your code here!
 			// ***************************************************************
--- java/itm/video/VideoMetadataGenerator.java
+++ java/itm/video/VideoMetadataGenerator.java
@@ -7,25 +7,15 @@ package itm.video;
 
 import itm.model.MediaFactory;
 import itm.model.VideoMedia;
+import itm.util.IOUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-
-import javax.media.ControllerEvent;
-import javax.media.ControllerListener;
-import javax.media.Manager;
-import javax.media.Processor;
-import javax.media.control.TrackControl;
-import javax.media.format.AudioFormat;
-import javax.media.format.VideoFormat;
-import javax.media.protocol.DataSource;
 
 import com.xuggle.xuggler.IContainer;
 import com.xuggle.xuggler.IStream;
 import com.xuggle.xuggler.IStreamCoder;
-import com.xuggle.xuggler.ICodec.Type;
 
 /**
  * This class reads video files, extracts metadata for both the audio and the
@@ -142,15 +132,49 @@ public class VideoMetadataGenerator {
 		// Fill in your code here!
 		// ***************************************************************
 		
+		VideoMedia media = null;
+		IContainer container = null;
 		
 		// create video media object
-		VideoMedia media = (VideoMedia) MediaFactory.createMedia(input);
+		media = (VideoMedia) MediaFactory.createMedia(input);
 
-		// set video and audio stream metadata 
+		// set video and audio stream metadata
+		container = IContainer.make();
+		container.open(input.getAbsolutePath(), IContainer.Type.READ, null);
+
+		for(int i = 0; i < container.getNumStreams(); i++) {
+			IStream stream = container.getStream(i);
+			IStreamCoder coder = stream.getStreamCoder();
+			
+			if(coder.getCodecType().toString().equals("CODEC_TYPE_AUDIO")) {
+				
+				media.setAudioCodec(coder.getCodec().toString());
+				media.setAudioCodecID(coder.getCodecID().toString());
+				media.setAudioChannels(coder.getChannels());
+				media.setAudioSampleRate(coder.getSampleRate());
+				media.setAudioBitRate(coder.getBitRate());
+				
+			} else if(coder.getCodecType().toString().equals("CODEC_TYPE_VIDEO")) {
+				
+				media.setVideoCodec(coder.getCodec().toString());
+				media.setVideoCodecID(coder.getCodecID().toString());
+				media.setVideoFrameRate(coder.getFrameRate().toString());
+				media.setVideoLength(container.getDuration());
+				media.setVideoHeight(coder.getHeight());
+				media.setVideoWidth(coder.getWidth());
+			}
+			
+			coder.close();
+		}
 		
 		// add video tag
+		media.addTag("video");
 
 		// write metadata
+		IOUtil.writeFile(media.serializeObject(), outputFile);
+		
+		// close container
+		container.close();
 		
 		return media;
 	}
@@ -161,7 +185,7 @@ public class VideoMetadataGenerator {
 	 */
 	public static void main(String[] args) throws Exception {
 
-		// args = new String[] {"./media/video", "./media/md"};
+		args = new String[] {"./media/video/panda.avi", "./media/md"};
 
 		if (args.length < 2) {
 			System.out.println("usage: java itm.video.VideoMetadataGenerator <input-video> <output-directory>");
