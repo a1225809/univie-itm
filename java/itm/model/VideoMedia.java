53826fcb8f325352d4f953da76fc50420f69dd2d
Pascal Attwenger <pascal.attwenger@gmail.com>
2014-05-14 21:59:57 +0200

    VideoMedia: formatting

--- java/itm/model/VideoMedia.java
+++ java/itm/model/VideoMedia.java
@@ -58,7 +58,11 @@ public class VideoMedia extends AbstractMedia {
 	}
 
 	public void setVideoCodec(String videoCodec) {
-		this.videoCodec = videoCodec;
+		if (videoCodec.startsWith("com.xuggle")) {
+			this.videoCodec = videoCodec.split("name=")[1].split(";")[0];
+		} else {
+			this.videoCodec = videoCodec;
+		}
 	}
 
 	public String getVideoCodecID() {
@@ -106,7 +110,11 @@ public class VideoMedia extends AbstractMedia {
 	}
 
 	public void setAudioCodec(String audioCodec) {
-		this.audioCodec = audioCodec;
+		if (audioCodec.startsWith("com.xuggle")) {
+			this.audioCodec = audioCodec.split("name=")[1].split(";")[0];
+		} else {
+			this.audioCodec = audioCodec;
+		}
 	}
 
 	public String getAudioCodecID() {
@@ -151,36 +159,25 @@ public class VideoMedia extends AbstractMedia {
 	public StringBuffer serializeObject() throws IOException {
 		StringWriter data = new StringWriter();
 		PrintWriter out = new PrintWriter(data);
+		
 		out.println("type: video");
 		StringBuffer sup = super.serializeObject();
 		out.print(sup);
 
 		/* video fields */
-
-		// ***************************************************************
-		// Fill in your code here!
-		// ***************************************************************
-
-		out.println();
-		out.println("Video-Metadata: ");
-		out.println();
-
 		out.println("videoCodec: " + this.videoCodec);
 		out.println("videoCodecID: " + this.videoCodecID);
 		out.println("videoFrameRate: " + this.videoFrameRate);
-		out.println("videoLength [sec]: " + this.videoLength);
-		out.println("videoHeight [pixel]: " + this.videoHeight);
-		out.println("videoWidth: [pixel]" + this.videoWidth);
-
-		out.println();
-		out.println("Audio-Metadata");
-		out.println();
-
+		out.println("videoLength: " + this.videoLength);
+		out.println("videoHeight: " + this.videoHeight);
+		out.println("videoWidth: " + this.videoWidth);
+		
+		/* audio fields */
 		out.println("audioCodec: " + this.audioCodec);
 		out.println("audioCodecID: " + this.audioCodecID);
 		out.println("audioChannels: " + this.audioChannels);
-		out.println("audioSampleRate [Hz]: " + this.audioSampleRate);
-		out.println("audioBitRate [kb/s]: " + this.audioBitRate);
+		out.println("audioSampleRate: " + this.audioSampleRate);
+		out.println("audioBitRate: " + this.audioBitRate);
 
 		return data.getBuffer();
 	}
@@ -198,11 +195,6 @@ public class VideoMedia extends AbstractMedia {
 		while ((line = br.readLine()) != null) {
 
 			/* video fields */
-
-			// ***************************************************************
-			// Fill in your code here!
-			// ***************************************************************
-
 			if (line.startsWith("videoCodec: ")) {
 				this.setVideoCodec(line.substring("videoCodec: ".length()));
 
@@ -210,20 +202,16 @@ public class VideoMedia extends AbstractMedia {
 				this.setVideoCodecID(line.substring("videoCodecID: ".length()));
 
 			} else if (line.startsWith("videoFrameRate: ")) {
-				this.setVideoFrameRate(line.substring("videoFramRate: "
-						.length()));
+				this.setVideoFrameRate(line.substring("videoFrameRate: ".length()));
 
-			} else if (line.startsWith("videoLength [sec]: ")) {
-				this.setVideoLength(Integer.parseInt(line
-						.substring("videoLength [sec]: ".length())) * 1000);
+			} else if (line.startsWith("videoLength: ")) {
+				this.setVideoLength(Integer.parseInt(line.substring("videoLength: ".length())) * 1000000);
 
-			} else if (line.startsWith("videoHeight [pixel]: ")) {
-				this.setVideoHeight(Integer.parseInt(line
-						.substring("videoHeight [pixel]: ".length())));
+			} else if (line.startsWith("videoHeight: ")) {
+				this.setVideoHeight(Integer.parseInt(line.substring("videoHeight: ".length())));
 
-			} else if (line.startsWith("videoWidth [pixel]: ")) {
-				this.setVideoWidth(Integer.parseInt(line
-						.substring("videoWidth [pixel]: ".length())));
+			} else if (line.startsWith("videoWidth: ")) {
+				this.setVideoWidth(Integer.parseInt(line.substring("videoWidth: ".length())));
 
 			} else if (line.startsWith("audioCodec: ")) {
 				this.setAudioCodec(line.substring("audioCodec: ".length()));
@@ -232,16 +220,13 @@ public class VideoMedia extends AbstractMedia {
 				this.setAudioCodecID(line.substring("audioCodecID: ".length()));
 
 			} else if (line.startsWith("audioChannels: ")) {
-				this.setAudioChannels(Integer.parseInt(line
-						.substring("audioChannels: ".length())));
+				this.setAudioChannels(Integer.parseInt(line.substring("audioChannels: ".length())));
 
-			} else if (line.startsWith("audioSampleRate [Hz]: ")) {
-				this.setAudioSampleRate(Integer.parseInt(line
-						.substring("audioSampleRate [Hz]: ".length())) / 1000);
+			} else if (line.startsWith("audioSampleRate: ")) {
+				this.setAudioSampleRate(Integer.parseInt(line.substring("audioSampleRate: ".length())) / 1000);
 
-			} else if (line.startsWith("audioBitRate [kb/s]: ")) {
-				this.setAudioBitRate(Integer.parseInt(line
-						.substring("audioBitRate [kb/s]: ".length())) * 1000);
+			} else if (line.startsWith("audioBitRate: ")) {
+				this.setAudioBitRate(Integer.parseInt(line.substring("audioBitRate: ".length())) * 1000);
 			}
 		}
 	}
